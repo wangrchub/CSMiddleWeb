fea241584daf47c7581755e048159690669e348a
wangrc1 <wangrc1@centaline.com.cn>
2017-02-20 10:36:32 +0800

    添加网站

---
+++ CSFirstMiddle/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSFirstMiddle.Startup))]
+namespace CSFirstMiddle
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
